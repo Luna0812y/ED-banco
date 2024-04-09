b618290f6ebb6a09109aad9fde032f9aac75e48b
Luna <102774024+Luna0812y@users.noreply.github.com>
2024-04-09 10:17:30 -0300

    main.c

--- main.c
+++ main.c
@@ -6,51 +6,51 @@
 int main(){
     Banco *banco = bancoNovo();
     InicializarBanco(banco);
-    Extrato *novoExtrato = (Extrato*)malloc(10 * sizeof(Extrato));
+    Extrato *novoExtrato = (Extrato*)malloc(sizeof(Extrato));
     novoExtrato->num_transacoes = 0;
 
-
     int qtd_contas = 0;
-    
+
     char *menu = criarMenuBanco(banco);
     int opcao;
     printf("%s", menu);
     scanf("%d", &opcao);
 
-
     while(opcao != 0){
         if (opcao == 1){
-            CriarConta(banco, &qtd_contas);            
+            Conta novaConta;
+            CriarConta(&novaConta, banco, qtd_contas);  // Passando o endereço de novaConta
+            banco->contas = (Conta *)realloc(banco->contas, (qtd_contas + 1) * sizeof(Conta));
+            banco->contas[qtd_contas] = novaConta;
+            (qtd_contas)++;
 
-        }else if(opcao == 2){
+        } else if(opcao == 2){
             MostrarConta(banco, qtd_contas);
 
-        }else if (opcao == 3){
+        } else if (opcao == 3){
             EntrarConta(banco, qtd_contas, novoExtrato);
 
-        }else if (opcao == 4){
+        } else if (opcao == 4){
             TransacaoGlobal(banco, qtd_contas, novoExtrato);
 
-        }else if (opcao == 5){
+        } else if (opcao == 5){
             int num;
             printf("Digite o numero da conta que deseja excluir \n>> ");
             scanf("%d", &num);
             ExcluirConta(banco, num, qtd_contas);
 
-        }else{
+        } else {
             printf("Opcao Invalida");
 
         }
 
         printf("%s", menu);
         scanf("%d", &opcao);
-        
-        
     }
 
     free(banco->contas);
     free(banco);
+    free(novoExtrato);
 
     return 0;
 }
-
